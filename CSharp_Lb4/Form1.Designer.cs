f415ce39aaf778adb8c45bc98d737877598176cc
VLad345406 <vlad.piermiakov@gmail.com>
2023-03-05 15:57:13 +0200

    Start make DataGridView

--- CSharp_Lb4/Form1.Designer.cs
+++ CSharp_Lb4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSharp_Lb4
 {
-    partial class Form1
+    partial class SongsLibrary
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.songName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artistName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.albumName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.songName,
+            this.artistName,
+            this.genre,
+            this.albumName});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // songName
+            // 
+            this.songName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.songName.HeaderText = "Назва треку";
+            this.songName.Name = "songName";
+            this.songName.Width = 95;
+            // 
+            // artistName
+            // 
+            this.artistName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.artistName.HeaderText = "Ім\'я виконавця";
+            this.artistName.Name = "artistName";
+            this.artistName.Width = 99;
+            // 
+            // genre
+            // 
+            this.genre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.genre.HeaderText = "Жанр";
+            this.genre.Name = "genre";
+            this.genre.Width = 61;
+            // 
+            // albumName
+            // 
+            this.albumName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.albumName.HeaderText = "Назва альбому";
+            this.albumName.Name = "albumName";
+            this.albumName.Width = 101;
+            // 
+            // SongsLibrary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SongsLibrary";
+            this.Text = "Аудіо-бібліотека";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn songName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artistName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn albumName;
     }
 }
 
--- CSharp_Lb4/Form1.cs
+++ CSharp_Lb4/Form1.cs
@@ -10,11 +10,24 @@ using System.Windows.Forms;
 
 namespace CSharp_Lb4
 {
-    public partial class Form1 : Form
+    public partial class SongsLibrary : Form
     {
-        public Form1()
+        private void UpdateFont()
+        {
+            //Change cell font
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+            {
+                c.DefaultCellStyle.Font = new Font("Times new roman", 30F, GraphicsUnit.Pixel);
+            }
+        }
+
+        public SongsLibrary()
         {
             InitializeComponent();
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
         }
     }
 }
